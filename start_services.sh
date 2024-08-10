f0d99fdb55137d26febd91c69eb7006c0cdf368d
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-10 13:00:27 -0500

    start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if Docker command is installed
+if ! command -v docker &> /dev/null; then
+  echo "Docker command is not installed. Please install Docker and try again."
+  exit 1
+fi
+
 # Start the Docker Compose services
 docker compose up -d db
 docker compose run --rm backend sh -c "(bundle check || bundle install) && bundle exec rails server -b 0.0.0.0 -p 3000"
