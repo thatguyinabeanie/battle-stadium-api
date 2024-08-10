94ba8c11b05886a8e179535703d36ef1e5397ddc
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-09 22:56:51 -0500

    we have something that kinda works. still broken. but progress is progress

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
+
         <h1 className={title()}>Make&nbsp;</h1>
         <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
         <br />
