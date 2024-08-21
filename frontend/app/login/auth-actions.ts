ada88efccbef30cfcb950ec57b827c5d11012155
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-21 17:48:23 -0500

    working oauth login workflow

---
+++ frontend/app/login/auth-actions.ts
@@ -0,0 +1,19 @@
+// authActions.ts
+"use server";
+
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
+import { redirect } from "next/navigation";
+
+const SIGNIN_ERROR_URL = "/";
+
+export async function handleSignIn (providerId: string) {
+  try {
+    await signIn(providerId);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
+    }
+    throw error;
+  }
+}
