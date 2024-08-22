60bdb16b5f63b7afe6a85c1d5ea9fd38a2825cf5
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-22 13:03:46 -0500

    refactor

---
+++ frontend/lib/server-actions/sign-in.ts
@@ -0,0 +1,20 @@
+// authActions.ts
+"use server";
+
+import { AuthError } from "next-auth";
+import { redirect } from "next/navigation";
+
+import { signIn as handleSignIn } from "@/auth";
+
+const SIGNIN_ERROR_URL = "/";
+
+export async function signIn(providerId: string) {
+  try {
+    await handleSignIn(providerId, { redirectTo: "/dashboard" });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`);
+    }
+    throw error;
+  }
+}
