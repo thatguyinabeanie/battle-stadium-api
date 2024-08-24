59d1c624af1b8a383013ee1cbceefab4e2d403af
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-24 09:37:38 -0500

    global bun

---
+++ frontend/global.d.ts
@@ -0,0 +1,23 @@
+import {
+  describe as bunDescribe,
+  it as bunIt,
+  expect as bunExpect,
+  test as bunTest
+} from "bun:test";
+
+import * as bun from "bun:test";
+
+declare global {
+  const describe: typeof bunDescribe;
+  const it: typeof bunIt;
+  const test: typeof bunTest;
+  const expect: typeof bunExpect;
+  // Add other globals as needed
+  const beforeAll: typeof globalThis.beforeAll;
+  const afterAll: typeof globalThis.afterAll;
+  const beforeEach: typeof globalThis.beforeEach;
+  const afterEach: typeof globalThis.afterEach;
+  const bun: typeof bun;
+}
+
+export { };
---
+++ frontend/test-setup.ts
@@ -0,0 +1,26 @@
+import { GlobalRegistrator } from "@happy-dom/global-registrator";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import {
+  describe, it, expect, beforeAll, afterAll, beforeEach, afterEach, test
+} from "bun:test";
+
+import * as bun from "bun:test";
+
+const oldConsole = console;
+GlobalRegistrator.register();
+window.console = oldConsole;
+
+
+expect.extend(matchers);
+
+Object.assign(global, {
+  bun,
+  describe,
+  test,
+  it,
+  expect,
+  beforeAll,
+  afterAll,
+  beforeEach,
+  afterEach,
+});
