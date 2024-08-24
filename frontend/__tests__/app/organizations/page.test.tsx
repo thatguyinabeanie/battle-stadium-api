c07c624ed501de46c7074baa8a9ffe42f0945e59
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-24 10:34:04 -0500

    organizations page test

--- frontend/__tests__/app/organizations/page.test.tsx
+++ frontend/__tests__/app/organizations/page.test.tsx
@@ -2,12 +2,16 @@ import { render, screen } from "@testing-library/react";
 
 import OrganizationPage from "@/app/organizations/page";
 import BattleStadiumAPI from "@/lib/battle-stadium-api";
+import { OrganizationFactory } from "@/factories";
 describe("Organizations Page", () => {
   it("renders the correct text", async () => {
-    spyOn(BattleStadiumAPI.Organizations, "list").mockResolvedValue([]);
+
+    const org = OrganizationFactory.build();
+
+    spyOn(BattleStadiumAPI.Organizations, "list").mockResolvedValue([org]);
 
     render(await OrganizationPage());
-    const text = screen.getByText(/Organizations/i);
+    const text = screen.getByText(org.name);
 
     expect(text).toBeDefined();
   });
