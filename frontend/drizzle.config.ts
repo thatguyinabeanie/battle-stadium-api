30f30196045689ffbd926105410d846145e73857
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-24 17:29:13 -0500

    drizzle

---
+++ frontend/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "drizzle-kit";
+import { config } from 'dotenv';
+config();
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./src/schema.ts",
+  out: "./drizzle",
+  verbose: true,
+  strict: true,
+  dbCredentials: {
+    url: "postgres://postgres:postgres@db:5432/fuecoco-db-dev",
+  }
+});
---
+++ frontend/lib/drizzleClient.ts
@@ -0,0 +1,17 @@
+// lib/drizzleClient.ts
+import { Pool } from 'pg';
+import { drizzle } from 'drizzle-orm';
+
+const pool = new Pool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
+const db = drizzle(pool);
+
+export default db;
