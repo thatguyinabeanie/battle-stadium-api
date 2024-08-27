8d8b0cd7412c3719139feb97bdce86271b208f97
thatguyinabeanie <thatguyin@beanie.gg>
2024-08-27 18:56:26 -0500

    fix-immutable-error

--- frontend/app/[...nextauth]/route.ts
+++ frontend/app/[...nextauth]/route.ts
@@ -1,3 +1,2 @@
 import { handlers } from "@/auth";
-export const runtime = "edge";
 export const { GET, POST } = handlers;
--- frontend/lib/server-actions/sign-up.tsx
+++ frontend/lib/server-actions/sign-up.tsx
@@ -7,18 +7,19 @@ import { credentialsSignIn } from "@/lib/server-actions/sign-in";
 
 export default function SignUp() {
   return (
-    <p className="mt-3 text-center text-small">
-      Need to create an account?&nbsp;
-      <m.form action={credentialsSignIn}>
-        <Button
-          fullWidth
-          startContent={<Icon className="text-default-500" icon={"logos:people"} width={20} />}
-          type="submit"
-          variant="flat"
-        >
-          <span>Sign Up</span>
-        </Button>
-      </m.form>
-    </p>
+    <m.form action={ credentialsSignIn }>
+      <p className="mt-3 text-center text-small">
+        Need to create an account?&nbsp;
+
+          <Button
+            fullWidth
+            startContent={<Icon className="text-default-500" icon={"logos:people"} width={20} />}
+            type="submit"
+            variant="flat"
+          >
+            <span>Sign Up</span>
+          </Button>
+      </p>
+    </m.form>
   );
 }
