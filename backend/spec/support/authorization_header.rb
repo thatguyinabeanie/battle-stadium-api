f9bf26aa80d3b199b952bb64de6ef9659551e204
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-05 15:36:00 -0500

    authorization

---
+++ backend/spec/support/authorization_header.rb
@@ -0,0 +1,24 @@
+require "factory_bot"
+class AuthorizationHeader
+  def self.bearer_token(user:)
+    session = FactoryBot.create(:session, user:)
+
+    jwt = JsonWebToken.encrypt(
+      {
+        session: {
+          sessionToken: session.token,
+          user: {
+            id: session.user.id,
+            email: session.user.email,
+            firstName: session.user.first_name,
+            lastName: session.user.last_name,
+            pronouns: session.user.pronouns,
+            emailVerified: session.user.email_verified_at
+          }
+        }
+      }
+    )
+
+    "Bearer #{jwt}"
+  end
+end
