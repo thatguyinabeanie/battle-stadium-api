578a8f8a73cd1dca4e8424ff0da890443aa11243
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-04 21:28:17 -0500

    props

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/header";
 import MainPageLayout from "@/components/main-page-layout";
 export default function Home() {
   return (
-    <MainPageLayout>
+    <MainPageLayout title="Battle Stadium">
       <Header disabled title="Home" />
       <NextJSHome />
     </MainPageLayout>
