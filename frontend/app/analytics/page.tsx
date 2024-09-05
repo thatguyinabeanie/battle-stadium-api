8ef651ad8c8c7c9cfe4c7d4ea23b036b7bfc3ba8
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-04 21:36:28 -0500

    fix unreachable code

--- frontend/app/analytics/page.tsx
+++ frontend/app/analytics/page.tsx
@@ -1,11 +1,7 @@
 export default async function Dashboard() {
-  try {
-    return (
-      <div>
-        <h1>Analytics</h1>
-      </div>
-    );
-  } catch (error) {
-    return <div>Something went wrong</div>;
-  }
+  return (
+    <div>
+      <h1>Analytics</h1>
+    </div>
+  );
 }
