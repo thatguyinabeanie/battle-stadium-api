2fabb2382da132640f3e2d0401dc0c28c0359221
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-05 19:48:44 -0500

    formatting

--- frontend/components/organizations/OrganizationCard.tsx
+++ frontend/components/organizations/OrganizationCard.tsx
@@ -12,58 +12,53 @@ export type PlaceListItemProps = Omit<React.HTMLAttributes<HTMLDivElement>, "id"
   organization: OrganizationDetails;
 };
 
-const OrganizationCard = React.forwardRef<HTMLDivElement, PlaceListItemProps>(
-  ({ organization, removeWrapper, className, isLoading, ...rest }, ref) => {
-    return (
-      <div
-        ref={ref}
-        className={cn(
-          "relative flex w-full flex-col gap-3",
-          {
-            "rounded-none bg-background shadow-none": removeWrapper,
-          },
-          className,
-        )}
-        {...rest}
+export default function OrganizationCard(props: PlaceListItemProps) {
+  const { organization, removeWrapper, className, isLoading, ...rest } = props;
+
+  return (
+    <div
+      className={cn(
+        "relative flex w-full flex-col gap-3",
+        {
+          "rounded-none bg-background shadow-none": removeWrapper,
+        },
+        className,
+      )}
+      {...rest}
+    >
+      <Link
+        key={organization.id}
+        aria-label={`View details for ${organization.name}`}
+        href={`/organizations/${organization.id}`}
       >
-        <Link
-          key={organization.id}
-          aria-label={`View details for ${organization.name}`}
-          href={`/organizations/${organization.id}`}
-        >
-          <Image
-            isBlurred
-            isZoomed
-            alt={organization.name}
-            aria-label={organization.name}
-            className="aspect-square w-full hover:scale-110"
-            isLoading={isLoading}
-            src={organization.logoUrl ?? "/pokemon/vgc.png"}
-          />
-        </Link>
+        <Image
+          isBlurred
+          isZoomed
+          alt={organization.name}
+          aria-label={organization.name}
+          className="aspect-square w-full hover:scale-110"
+          isLoading={isLoading}
+          src={organization.logoUrl ?? "/pokemon/vgc.png"}
+        />
+      </Link>
 
-        <div className="mt-1 flex flex-col gap-2 px-1">
-          {isLoading ? (
-            <LoadingPlaceholder />
-          ) : (
-            <>
-              <div className="flex items-start justify-between gap-1">
-                <h3 className="text-small font-medium text-default-700" data-testid="org-name">
-                  {organization.name}
-                </h3>
-              </div>
-              {organization?.description ? (
-                <p className="text-small text-default-500">{organization?.description}</p>
-              ) : null}
-              <p className="text-small font-medium text-default-500">$100</p>
-            </>
-          )}
-        </div>
+      <div className="mt-1 flex flex-col gap-2 px-1">
+        {isLoading ? (
+          <LoadingPlaceholder />
+        ) : (
+          <>
+            <div className="flex items-start justify-between gap-1">
+              <h3 className="text-small font-medium text-default-700" data-testid="org-name">
+                {organization.name}
+              </h3>
+            </div>
+            {organization?.description ? (
+              <p className="text-small text-default-500">{organization?.description}</p>
+            ) : null}
+            <p className="text-small font-medium text-default-500">$100</p>
+          </>
+        )}
       </div>
-    );
-  },
-);
-
-OrganizationCard.displayName = "OrganizationCard";
-
-export default OrganizationCard;
+    </div>
+  );
+}
