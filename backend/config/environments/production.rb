6693e33f778836eaf103c807d7338e8608731752
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-05 20:03:32 -0500

    add host

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -90,6 +90,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
+    "api-battle-stadium-gg.onrender.com",
     "localhost" # Allow requests from example.com
     # /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   ]
