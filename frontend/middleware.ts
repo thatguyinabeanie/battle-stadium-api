f09fbcd4612104bbb28c6fe97be0e53c243683f7
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-07 14:39:27 -0500

    bun fix

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -8,8 +8,6 @@ export default clerkMiddleware((auth, request) => {
   }
 });
 
-
-
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
