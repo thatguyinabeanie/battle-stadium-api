6f56bf766caa29675149e1cb0aaac34f2a760a20
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-07 01:52:56 -0500

    cleanup unused packages

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,20 +1,16 @@
 import "@/styles/globals.css";
 
-import { NextUIProvider, ReactQueryClientProvider, ThemesProvider } from "@/components/providers";
+import { NextUIProvider, ThemesProvider } from "@/components/providers";
 import { ChildrenProps } from "@/types";
 import { CurrentUserContextProvider } from "@/lib/context/current-user";
-
-const initialIsOpen = process.env.NODE_ENV === "development";
 
 export default async function Providers({ children }: Readonly<ChildrenProps>) {
   return (
     <NextUIProvider>
       <ThemesProvider attribute="class" defaultTheme="dark">
-        <ReactQueryClientProvider initialIsOpen={initialIsOpen}>
-          <div className="flex h-dvh w-full">
-            <CurrentUserContextProvider initCurrentUser={null}>{children}</CurrentUserContextProvider>
-          </div>
-        </ReactQueryClientProvider>
+        <div className="flex h-dvh w-full">
+          <CurrentUserContextProvider initCurrentUser={null}>{children}</CurrentUserContextProvider>
+        </div>
       </ThemesProvider>
     </NextUIProvider>
   );
--- frontend/components/providers/index.ts
+++ frontend/components/providers/index.ts
@@ -1,7 +1,5 @@
-import ReactQueryClientProvider, { ReactQueryClientProviderProps } from "./react-query-client-provider";
 import NextUIProvider from "./next-ui-provider";
 import ThemesProvider from "./themes-provider";
 
-export { ReactQueryClientProvider, NextUIProvider, ThemesProvider };
+export { NextUIProvider, ThemesProvider };
 
-export type { ReactQueryClientProviderProps };
