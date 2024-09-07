9e483c7cba56660fec5853abe89400ad93eecf66
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-07 11:17:41 -0500

    /user-profile

---
+++ frontend/app/user-profile/layout.tsx
@@ -0,0 +1,6 @@
+import MainPageLayout from "@/components/main-page-layout";
+import { ChildrenProps } from "@/types";
+
+export default function DashboardLayout({ children }: Readonly<ChildrenProps>) {
+  return <MainPageLayout>{children}</MainPageLayout>;
+}
