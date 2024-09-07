465c4addec8630e55678a8ae22651be2ae8b7739
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-07 15:54:53 -0500

    conditional middleware

--- frontend/app/organizations/page.tsx
+++ frontend/app/organizations/page.tsx
@@ -1,38 +1,11 @@
 import React from "react";
 
 import { cn } from "@/lib/utils";
-import { prisma } from "@/prisma/client";
 import OrganizationCard from "@/components/organizations/OrganizationCard";
-import { OrganizationDetails } from "@/lib/api";
+import BattleStadiumAPI, { OrganizationDetails } from "@/lib/api";
 
 const OrganizationsPage = async () => {
-  const orgs = (
-    await prisma.organizations.findMany({
-      include: {
-        users: true,
-      },
-    })
-  ).map((org) => ({
-    ...org,
-    owner: org.users
-      ? {
-          ...org.users,
-          username: org.users.username ?? "",
-        }
-      : {
-          id: "",
-          email: "",
-          username: "",
-          created_at: new Date(),
-          updated_at: new Date(),
-          encrypted_password: "",
-          reset_password_token: null,
-          reset_password_sent_at: null,
-          admin: false,
-          pronouns: "",
-        },
-    logoUrl: org.logo_url ?? "/pokemon/vgc.png",
-  }));
+  const orgs = await BattleStadiumAPI().Organizations.list();
 
   return (
     <div
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -3,8 +3,10 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
 
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
+  if (process.env.NODE_ENV === "production") {
+    if (!isPublicRoute(request)) {
+      auth().protect();
+    }
   }
 });
 
