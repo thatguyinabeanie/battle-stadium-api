616e2dac3a9fbb5ea6dde4bd408b147c775502ff
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-07 09:44:43 -0500

    no explicit any

--- frontend/lib/auth/utils.ts
+++ frontend/lib/auth/utils.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 /**
  * Returns a Response object with a JSON body
  */
-export function jsonResponse(status: number, data: any, init?: ResponseInit) {
+export function jsonResponse(status: number, data: unknown, init?: ResponseInit) {
   return new NextResponse(JSON.stringify(data), {
     ...init,
     status,
