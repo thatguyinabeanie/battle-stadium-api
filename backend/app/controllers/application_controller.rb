a1c11b26e1cef3e42211f660dd7abc89887743c4
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-08 15:15:52 -0500

    fixed some tests

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -4,17 +4,15 @@ require "clerk"
 require_relative "../../lib/clerk_jwt"
 
 class ApplicationController < ActionController::Base
-  include Clerk::Authenticatable
   attr_reader :current_user
+  include Clerk::Authenticatable
   include Pundit::Authorization
 
-  skip_before_action :verify_authenticity_token
   after_action :verify_authorized
 
   before_action :authenticate_clerk_user!
+  skip_before_action :authenticate_clerk_user!, only: %i[index show]
 
-  skip_before_action :verify_clerk_session, only: %i[index show]
-  before_action :set_no_cache_headers
 
   def index
     authorize self.class, :index?
@@ -34,13 +32,5 @@ class ApplicationController < ActionController::Base
 
   def pundit_user
     current_user
-  end
-
-  private
-
-  def set_no_cache_headers
-    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
 end
