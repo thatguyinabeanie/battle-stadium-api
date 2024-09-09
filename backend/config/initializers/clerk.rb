e3e0b2423cc671f9071013dd67a5d9429fddeda1
Gabe Mendoza <thatguyin@beanie.gg>
2024-09-08 21:04:46 -0500

    Site details (#300)
    * update description
    
    * explicit api key and base_url
    
    * explicit

--- backend/config/initializers/clerk.rb
+++ backend/config/initializers/clerk.rb
@@ -1,7 +1,10 @@
 require "clerk"
 
 Clerk.configure do |c|
+  c.api_key = ENV["CLERK_SECRET_KEY"]
+
+  c.base_url = "https://api.clerk.com" if Rails.env.production?
+
   c.logger = Logger.new(STDOUT) # if omitted, no logging
-  c.middleware_cache_store = ActiveSupport::Cache::FileStore.new("/tmp/clerk_middleware_cache") # if omitted: no caching
-  # c.excluded_routes ["/foo", "/bar/*"]
+  c.middleware_cache_store = ActiveSupport::Cache::FileStore.new("/tmp/clerk_middleware_cache")
 end
