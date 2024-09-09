46c99ea342dfdb482f8d46e1bca828ff5d5d49d2
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-08 20:34:16 -0500

    update description

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -2,5 +2,5 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Battle Stadium",
-  description: "Make beautiful websites regardless of your design experience.",
+  description: "A modern tournament platform. Built by the community, for the community.",
 };
