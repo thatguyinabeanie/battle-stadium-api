34439b3111e4ebc35d391dff9ef5b7e060ee6e85
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-08 21:02:51 -0500

    explicit

--- backend/config/initializers/clerk.rb
+++ backend/config/initializers/clerk.rb
@@ -2,7 +2,9 @@ require "clerk"
 
 Clerk.configure do |c|
   c.api_key = ENV["CLERK_SECRET_KEY"]
-  c.base_url = "https://api.clerk.com"
+
+  c.base_url = "https://api.clerk.com" if Rails.env.production?
+
   c.logger = Logger.new(STDOUT) # if omitted, no logging
   c.middleware_cache_store = ActiveSupport::Cache::FileStore.new("/tmp/clerk_middleware_cache")
 end
