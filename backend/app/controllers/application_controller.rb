f987dcb62e49dc93eee63020967333e2f6444fbc
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-08 21:52:30 -0500

    small fixes

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -28,7 +28,8 @@ class ApplicationController < ActionController::Base
 
   def authenticate_clerk_user!
     @current_user = ClerkJwt::Session.authenticate!(request:)
-  rescue ClerkJwt::Session::NoAuthorizationHeader, ClerkJwt::Session::VerificationError, ClerkJwt::Session::InvalidSessionToken => e
+  rescue StandardError => e
+    Rails.logger.error("authenticate_clerk_user! error: #{e.message}")
     render json: { error: e.message }, status: :unauthorized
   end
 
