89d846cdcb6033ced549d2950600488080843304
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-08 19:35:57 -0500

    fix export

--- frontend/lib/api/index.ts
+++ frontend/lib/api/index.ts
@@ -2,6 +2,5 @@ export * from "./generated-api-client/apis";
 export * from "./generated-api-client/models";
 export * from "./generated-api-client/runtime";
 export * from "./battle-stadium-api";
-export * from "./config";
 import BattleStadiumAPI from "./battle-stadium-api";
 export default BattleStadiumAPI;
