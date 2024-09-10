61a59d4a6e394c16d79954e3c83d7a247dd98925
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-10 00:11:34 -0500

    save

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -105,7 +105,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 10000 }
 
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  config.public_file_server.enabled = false
 
   config.secret_key_base = ENV["AUTH_SECRET"]
 end
