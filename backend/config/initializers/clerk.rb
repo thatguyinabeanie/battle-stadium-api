9a95b2deca6d71434af8b3ebd7097739f211caca
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-09 21:43:52 -0500

    remove clerk initializer

--- backend/config/initializers/clerk.rb
+++
@@ -1,6 +0,0 @@
-require "clerk"
-
-Clerk.configure do |c|
-  c.logger = Logger.new(STDOUT) # if omitted, no logging
-  c.middleware_cache_store = ActiveSupport::Cache::FileStore.new("/tmp/clerk_middleware_cache") # if omitted: no caching
-end
