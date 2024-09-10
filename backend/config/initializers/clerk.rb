891e47b494e9a2d46cd31a614daed0f2d02dad59
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-10 00:32:30 -0500

    no clerk

--- backend/config/initializers/clerk.rb
+++
@@ -1,8 +0,0 @@
-require "clerk"
-
-Clerk.configure do |c|
-  c.api_key = ENV.fetch("CLERK_SECRET_KEY", nil)
-  c.base_url = "https://api.clerk.com/v1/"
-  c.logger = Logger.new(STDOUT) # if omitted, no logging
-  c.middleware_cache_store = Rails.cache # if omitted: no caching
-end
