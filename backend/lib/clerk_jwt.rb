48a383e425d4f768c492f6f966492f4bafdcc38a
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-09 23:46:35 -0500

    scale it back. then drip feed it back in

--- backend/lib/clerk_jwt.rb
+++ backend/lib/clerk_jwt.rb
@@ -1,4 +1,4 @@
-# require "clerk"
+require "clerk"
 
 module ClerkJwt
   module Webhook
@@ -130,14 +130,9 @@ module ClerkJwt
         raise NoAuthorizationHeader, "Authorization header missing or malformed"  unless session_token
 
         begin
-          # clerk = Clerk::SDK.new(
-          #   api_key: ENV["CLERK_SECRET_KEY"],
-          #   logger: Logger.new(STDOUT),
-          # )
-          #
-          # return clerk.verify_token(session_token,)
-          #
-          nil
+          clerk = Clerk::SDK.new(api_key: ENV["CLERK_SECRET_KEY"], logger: Logger.new(STDOUT))
+
+          return clerk.verify_token(session_token)
         rescue StandardError => e
           raise VerificationError, e.message
         end
