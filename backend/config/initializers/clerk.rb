a7cf6ff9bab5c8d65eb1698a30ac3624d36b40f8
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-10 00:01:22 -0500

    com over dev

--- backend/config/initializers/clerk.rb
+++ backend/config/initializers/clerk.rb
@@ -1,6 +1,6 @@
 Clerk.configure do |c|
   c.api_key = ENV.fetch("CLERK_SECRET_KEY", nil)
-  c.base_url = "https://api.clerk.dev/v1/"
+  c.base_url = "https://api.clerk.com/v1/"
   c.publishable_key = ENV.fetch("CLERK_PUBLIC_KEY", nil)
   c.logger = Logger.new(STDOUT) # if omitted, no logging
   c.middleware_cache_store = Rails.cache # if omitted: no caching
