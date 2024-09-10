c070d164e6ebf73b60bedfc3321090ef70644eb9
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-09 23:14:05 -0500

    i am at my wits end

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 require "pundit"
-require_relative "../../lib/clerk_jwt"
+# require_relative "../../lib/clerk_jwt"
 
 class ApplicationController < ActionController::Base
   attr_reader :current_user
   include Pundit::Authorization
 
   after_action :verify_authorized
-  before_action :authenticate_clerk_user!
-  skip_before_action :authenticate_clerk_user!, only: %i[index show]
+  # before_action :authenticate_clerk_user!
+  # skip_before_action :authenticate_clerk_user!, only: %i[index show]
 
   def self.policy_class
     ::ApplicationPolicy
@@ -23,13 +23,13 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def authenticate_clerk_user!
-    @current_user = ClerkJwt::Session.authenticate!(request:)
-    @current_user
-  rescue StandardError => e
-    Rails.logger.info "Failed to authenticate user: #{e.message}"
-    render json: { error: e.message }, status: :unauthorized
-  end
+  # def authenticate_clerk_user!
+  #   @current_user = ClerkJwt::Session.authenticate!(request:)
+  #   @current_user
+  # rescue StandardError => e
+  #   Rails.logger.info "Failed to authenticate user: #{e.message}"
+  #   render json: { error: e.message }, status: :unauthorized
+  # end
 
   def pundit_user
     current_user
--- backend/lib/clerk_jwt.rb
+++ backend/lib/clerk_jwt.rb
@@ -1,4 +1,4 @@
-require "clerk"
+# require "clerk"
 
 module ClerkJwt
   module Webhook
@@ -130,11 +130,14 @@ module ClerkJwt
         raise NoAuthorizationHeader, "Authorization header missing or malformed"  unless session_token
 
         begin
-          clerk = Clerk::SDK.new(
-            api_key: ENV["CLERK_SECRET_KEY"],
-            logger: Logger.new(STDOUT),
-          )
-          return clerk.verify_token(session_token,)
+          # clerk = Clerk::SDK.new(
+          #   api_key: ENV["CLERK_SECRET_KEY"],
+          #   logger: Logger.new(STDOUT),
+          # )
+          #
+          # return clerk.verify_token(session_token,)
+          #
+          nil
         rescue StandardError => e
           raise VerificationError, e.message
         end
