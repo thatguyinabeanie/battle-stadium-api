77dc65d517e283929d5626ae8e1bf667094d0029
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-11 16:54:37 -0500

    fix sign in and sing up page

--- frontend/app/sign-in/[[...rest]]/page.tsx
+++ frontend/app/sign-in/[[...rest]]/page.tsx
@@ -10,7 +10,6 @@ export default function SignInPage() {
     return <div>You are signed in.. </div>;
   }
 
-export default function SignInPage() {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 pb-10 pt-6 shadow-small">
