a1bfba154953902cbed4380bd0937e79ca630f85
Gabe Mendoza <thatguyin@beanie.gg>
2024-09-11 16:56:15 -0500

    put clerk signin and signup pages back (#322)

--- frontend/app/sign-up/[[...rest]]/page.tsx
+++ frontend/app/sign-up/[[...rest]]/page.tsx
@@ -3,18 +3,12 @@
 import React from "react";
 import { SignUp, useUser } from "@clerk/nextjs";
 
-export default function Component() {
+export default function SignUpPage() {
   const { user } = useUser();
 
   if (user) {
     return <div>You are signed in.. </div>;
   }
 
-  return (
-    <div className="flex h-full w-full items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 pb-10 pt-6 shadow-small">
-        <SignUp />
-      </div>
-    </div>
-  );
+  return <SignUp path="/sign-up" routing="path" />;
 }
