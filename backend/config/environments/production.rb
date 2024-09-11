37e105c5bb79c9046d63358b9639d7e782febc45
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-11 14:17:10 -0500

    pr

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
     "api.battlestadium.gg",
     "www.battlestadium.gg",
     "battle-stadium-api.onrender.com",
-    "battle-stadium-api-pr-313.onrender.com",
+    "battle-stadium-api-pr-317.onrender.com",
   ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
