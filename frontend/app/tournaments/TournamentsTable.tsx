79c47bbd999e99a990589bffd99e028f618d10b7
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-12 23:44:50 -0500

    undo render cell

--- frontend/app/tournaments/TournamentsTable.tsx
+++ frontend/app/tournaments/TournamentsTable.tsx
@@ -8,7 +8,6 @@ import { components } from "@/lib/battle-stadium-api";
 export interface TournamentsTableProps {
   tournaments?: components["schemas"]["Tournament"][];
   disableColumns?: string[];
-  RenderCell: typeof getKeyValue;
 }
 
 const columns = [
@@ -34,8 +33,71 @@ const columns = [
   },
 ];
 
+function renderRegistration({
+  registration_start_at,
+  registration_end_at,
+  player_count,
+  player_cap,
+  late_registration,
+}: components["schemas"]["TournamentDetails"]) {
+  const currentTime = new Date();
 
-const TournamentsTable = ({ tournaments, disableColumns, RenderCell }: TournamentsTableProps) => {
+  if (!registration_start_at) {
+    return "Not Open";
+  }
+
+  const registrationStartAt = new Date(registration_start_at);
+
+  if (currentTime < registrationStartAt) {
+    return "Not Open";
+  }
+
+  const registrationEndAt = registration_end_at ? new Date(registration_end_at) : null;
+
+  if (currentTime >= registrationStartAt && registrationEndAt && currentTime < registrationEndAt) {
+    if (player_cap && player_count >= player_cap) {
+      return "Full";
+    }
+
+    return "Open";
+  }
+
+  if (registrationEndAt && currentTime > registrationEndAt) {
+    if (late_registration) {
+      return "Open";
+    }
+
+    return "Closed";
+  }
+}
+
+const renderCell: typeof getKeyValue = (row: components["schemas"]["TournamentDetails"], columnKey) => {
+  const { id, name, organization, start_at, player_count, player_cap } = row;
+
+  switch (columnKey) {
+    case "organization.name":
+      return <Link href={`/organizations/${organization.id}`}>{organization.name}</Link>;
+    case "start_at":
+      if (!start_at) {
+        return "TBD";
+      }
+      const startAt = new Date(start_at);
+      const date = startAt?.toLocaleString("en-US", { month: "short", day: "numeric", year: "numeric" });
+      const time = startAt?.toLocaleString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
+
+      return `${date} ${time}`;
+    case "name":
+      return <Link href={`/tournaments/${id}`}>{name}</Link>;
+    case "players":
+      return player_cap ? `${player_count}/${player_cap}` : player_count;
+    case "registration":
+      return renderRegistration(row);
+    default:
+      return null;
+  }
+};
+
+const TournamentsTable = ({ tournaments, disableColumns }: TournamentsTableProps) => {
   const [hydrated, setHydrated] = React.useState(false);
 
   React.useEffect(() => {
@@ -54,7 +116,7 @@ const TournamentsTable = ({ tournaments, disableColumns, RenderCell }: Tournamen
 
       <TableBody items={tournaments}>
         {(item) => (
-          <TableRow key={item.id}>{(columnKey) => <TableCell> <RenderCell row={item} columnKey={columnKey}/></TableCell>}</TableRow>
+          <TableRow key={item.id}>{(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}</TableRow>
         )}
       </TableBody>
     </Table>
--- frontend/app/tournaments/page.tsx
+++ frontend/app/tournaments/page.tsx
@@ -6,21 +6,31 @@ import { BattleStadiumAPI } from "@/lib/battle-stadium-api";
 
 import TournamentsTable from "./TournamentsTable";
 
-import RenderCell from "./RenderCell";
 
 export const metadata: Metadata = {
   title: "Tournaments",
 };
 
 async function listTournaments() {
-  return await BattleStadiumAPI.GET("/tournaments");
+  return await BattleStadiumAPI.GET("/tournaments", {
+    next: {
+      tags: ["tournaments"],
+      // revalidate: 60*60*2,
+    },
+    params: {
+      query: {
+        per_page: 10,
+        page: 0,
+      }
+    },
+  });
 }
 
 const Tournaments = async () => {
   const response = await listTournaments();
   const tours = response.data?.tournaments;
 
-  return <TournamentsTable tournaments={tours} RenderCell={RenderCell}/>;
+  return <TournamentsTable tournaments={tours} />;
 };
 
 export default Tournaments;
