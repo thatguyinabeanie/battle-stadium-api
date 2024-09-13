f2c8468799073128af14d19dad85e0498176b2fd
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-12 22:21:19 -0500

    entrypoint

--- frontend/app/organizations/[organizationId]/page.tsx
+++ frontend/app/organizations/[organizationId]/page.tsx
@@ -3,11 +3,9 @@ import TournamentsTable from "@/app/tournaments/TournamentsTable";
 import OrganizationCard from "@/components/organizations/OrganizationCard";
 import { BattleStadiumAPI } from "@/lib/battle-stadium-api";
 
-export interface OrganizationDetailPageProps {
-  params: {
-    organizationId: number;
-  };
-}
+export const revalidate = 300;
+export const dynamicParams = true;
+
 export async function generateMetadata({ params }: { params: { id: string } }) {
   const { data: organization } = await getOrganization(parseInt(params.id));
 
@@ -26,27 +24,6 @@ async function getOrganization(organizationId: number) {
   });
 }
 
-// Next.js will invalidate the cache when a
-// request comes in, at most once every 60 seconds.
-export const revalidate = 300;
-
-// We'll prerender only the params from `generateStaticParams` at build time.
-// If a request comes in for a path that hasn't been generated,
-// Next.js will server-render the page on-demand.
-export const dynamicParams = true; // or false, to 404 on unknown paths
-
-export async function generateStaticParams() {
-  const { data: organizations } = await BattleStadiumAPI.GET("/organizations", {
-    next: { tags: ["organizations"] },
-  });
-
-  return organizations?.map((organization) => ({
-    params: {
-      organizationId: organization.id.toString(),
-    },
-  }));
-}
-
 async function getTournaments(organizationId: number) {
   return await BattleStadiumAPI.GET("/organizations/{org_id}/tournaments", {
     params: {
@@ -57,9 +34,18 @@ async function getTournaments(organizationId: number) {
   });
 }
 
-export default async function OrganizationDetailPage({ params: { organizationId } }: OrganizationDetailPageProps) {
-  const { data: organization } = await getOrganization(organizationId);
-  const { data: tournaments } = await getTournaments(organizationId);
+export async function generateStaticParams() {
+  const { data: organizations } = await BattleStadiumAPI.GET("/organizations", {
+    next: { tags: ["organizations"] },
+  });
+
+  console.log('organizations', organizations);
+  return (organizations ?? []).map((organization) => ({ organizationId: organization.id }));
+}
+
+export default async function OrganizationDetailPage({ params }: { params: { organizationId: number } }) {
+  const { data: organization } = await getOrganization(params.organizationId);
+  const { data: tournaments } = await getTournaments(params.organizationId);
 
   return (
     <>
