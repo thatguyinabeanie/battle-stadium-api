1f27bd12ba9454b8dfa17f84eb27e9783500293b
Gabe Mendoza <thatguyin@beanie.gg>
2024-09-14 11:31:35 -0500

    backdrop blur (#344)

--- frontend/components/awesome-particles/awesome-particles.tsx
+++ frontend/components/awesome-particles/awesome-particles.tsx
@@ -3,14 +3,10 @@
 import * as React from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
-import { useTheme } from "next-themes";
 
 import getAwesomeParticlesOptions from "./getAwesomeParticlesOptions";
 
 export default function AwesomeParticles() {
-  const options = getAwesomeParticlesOptions();
-
-  const theme = useTheme();
   const [init, setInit] = React.useState(false);
 
   React.useEffect(() => {
@@ -19,10 +15,11 @@ export default function AwesomeParticles() {
     }).then(() => setInit(true));
   }, []);
 
-  if (!init || !theme.resolvedTheme?.includes("dark")) {
+  if (!init) {
     return <></>;
   }
 
-  // Rest of the code goes here
+  const options = getAwesomeParticlesOptions();
+
   return <Particles id="tsparticles" options={options} />;
 }
