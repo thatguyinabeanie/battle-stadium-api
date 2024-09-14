e65c947b818654b948a12dd6292bdd315017458a
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-13 23:40:11 -0500

    navbar settings link

--- frontend/components/navbar/settings.tsx
+++ frontend/components/navbar/settings.tsx
@@ -1,13 +1,16 @@
 import { NavbarItem, Button } from "@nextui-org/react";
 
 import { Icon } from "../client";
+import Link from "next/link";
 
 export default function Settings() {
   return (
     <NavbarItem className="hidden md:flex">
-      <Button isIconOnly radius="full" variant="light">
-        <Icon className="text-default-500" icon="solar:settings-linear" width={24} />
-      </Button>
+      <Link href="/settings" passHref>
+        <Button isIconOnly radius="full" variant="light">
+          <Icon className="text-default-500" icon="solar:settings-linear" width={24} />
+        </Button>
+      </Link>
     </NavbarItem>
   );
 }
