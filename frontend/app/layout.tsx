98797f45371a601f6ffd9db7f01858df7ffe0ca7
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-16 11:20:09 -0500

    tsc

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -12,7 +12,7 @@ import { ChildrenProps } from "@/types";
 import NavigationBar from "@/components/navbar/navbar";
 import Providers from "@/components/providers";
 
-import AwesomeParticles from "@/components/awesome-particles/awesome-particles";
+import AwesomeParticles from "@/components/awesome-particles";
 
 export const metadata: Metadata = {
   title: {
