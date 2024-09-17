2cd296ce80edd9db4a9e1d74a71d84daed38b4e9
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-16 22:29:02 -0500

    better responsive

--- frontend/app/organizations/[organizationId]/page.tsx
+++ frontend/app/organizations/[organizationId]/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { Card, CardBody, Image } from "@nextui-org/react";
 
 import TournamentsTable from "@/components/tournaments-table";
-import { BattleStadiumAPI } from "@/lib/battle-stadium-api";
+import { BattleStadiumAPI, components } from "@/lib/battle-stadium-api";
 
 export const revalidate = 200;
 export const dynamicParams = true;
@@ -36,22 +36,26 @@ export async function generateStaticParams() {
   return (orgs ?? []).map((organization) => ({ organizationId: organization.id.toString() }));
 }
 
+const organizationLogo = (organization: components["schemas"]["Organization"] | undefined, className: string|null= null) => (
+  <Image
+    alt={ organization?.name }
+    aria-label={ organization?.name }
+    className={`aspect-square gap-3 sm:h-[100px] sm:w-[100px] lg:h-[200px] lg:w-[200px] ${className}`}
+    src={ organization?.logo_url ?? "/pokemon/vgc.png" }
+  />
+);
+
 export default async function OrganizationDetailPage({ params }: Readonly<{ params: { organizationId: number } }>) {
   const { data: organization } = await getOrganization(params.organizationId);
   const { data: tournaments } = await getTournaments(params.organizationId);
 
-  const OrganizerLogo = <Image
-    alt={ organization?.name }
-    aria-label={ organization?.name }
-    className="aspect-square gap-3 sm:h-[100px] sm:w-[100px] lg:h-[200px] lg:w-[200px]"
-    src={ organization?.logo_url ?? "/pokemon/vgc.png" }
-  />;
+
 
   return (
     <div className="w-100 h-100">
       <Card isBlurred shadow="none">
         <CardBody className="flex flex-row justify-between">
-          {OrganizerLogo}
+          { organizationLogo(organization) }
 
           <div className="flex flex-col justify-between text-center mx-4">
             <h1 className="text-2xl font-semibold">{organization?.name}</h1>
@@ -59,7 +63,7 @@ export default async function OrganizationDetailPage({ params }: Readonly<{ para
             <p>[ICON LINKS TO SOCIAL MEDIA PROFILES]</p>
           </div>
 
-          { OrganizerLogo }
+          { organizationLogo(organization, "hidden md:flex") }
         </CardBody>
       </Card>
 
