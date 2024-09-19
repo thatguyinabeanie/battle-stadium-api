7dc8f20de8f6ec5cd5be4a9dbd8a7d8ee60e6eeb
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-19 11:51:09 -0500

    fix some specs

--- backend/spec/support/auth/token_verifier_mock.rb
+++ backend/spec/support/auth/token_verifier_mock.rb
@@ -12,6 +12,24 @@ module Auth
         let(:request_user) { create(:user) }
         let(:Authorization) { "Bearer #{SecureRandom.alphanumeric(25)}, Bearer #{SecureRandom.alphanumeric(25)}" }
 
+        shared_context "with Request Specs - Vercel OIDC Token Verification" do
+          before do
+            allow_token_verification
+          end
+        end
+        shared_context "with Request Specs - Clerk JWT + Vercel OIDC Token Verification" do
+          before do
+            allow_token_verification
+          end
+        end
+
+        shared_context "with Controller Specs - Clerk JWT + Vercel OIDC Token Verification" do
+          before do
+            allow_token_verification
+            request.headers["Authorization"] = "Bearer #{SecureRandom.alphanumeric(25)}, Bearer #{SecureRandom.alphanumeric(25)}"
+          end
+        end
+
         def session_data(request_user)
           {
             "userId" => request_user.clerk_users.first&.clerk_user_id,
@@ -29,32 +47,15 @@ module Auth
             .and_return(true)
         end
 
+        def allow_clerk_token_verification
+          allow(Auth::Clerk::TokenVerifier)
+            .to receive(:verify_token)
+            .and_return(true)
+        end
 
         def allow_token_verification
           allow_vercel_token_verification
-
-          allow(Auth::Clerk::TokenVerifier)
-            .to receive(:verify_token)
-            .and_return(session_data(request_user))
-        end
-
-        shared_context "with Request Specs - Vercel OIDC Token Verification" do
-          before do
-            allow_token_verification
-          end
-        end
-
-        shared_context "with Request Specs - Clerk JWT + Vercel OIDC Token Verification" do
-          before do
-            allow_token_verification
-          end
-        end
-
-        shared_context "with Controller Specs - Clerk JWT + Vercel OIDC Token Verification" do
-          before do
-            allow_token_verification
-            request.headers["Authorization"] = "Bearer #{SecureRandom.alphanumeric(25)}, Bearer #{SecureRandom.alphanumeric(25)}"
-          end
+          allow_clerk_token_verification
         end
       end
     end
