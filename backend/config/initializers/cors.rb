f17095eb145b6a931062179e05fbb1576975c986
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-23 14:33:23 -0500

    origins

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -10,6 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins [
       "battlestadium.gg",
       "www.battlestadium.gg",
+      "battle-stadium-git-fix-500-error-thatguyinabeanie.vercel.app",
     ]
 
     resource "*",
