e38b629ae268eb675f15c0dadac0b6bc8f0445f6
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-24 14:35:56 -0500

    specs

--- backend/app/channels/chat_channel.rb
+++ backend/app/channels/chat_channel.rb
@@ -1,13 +1,10 @@
+# app/channels/chat_channel.rb
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     stream_from "chat_#{params[:room_id]}"
   end
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-  end
-
   def speak(data)
-    ActionCable.server.broadcast("chat_#{params[:room]}", message: data["message"])
+    ActionCable.server.broadcast("chat_#{params[:room_id]}", {message: data["message"]})
   end
 end
--- backend/spec/channels/chat_channel_spec.rb
+++ backend/spec/channels/chat_channel_spec.rb
@@ -1,5 +1,31 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe ChatChannel, type: :channel do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe ChatChannel do
+  let(:room_id) { 1 }
+  let(:room) { "chat_#{room_id}" }
+
+  before do
+    stub_connection current_user: create(:user)
+  end
+
+  it "successfully subscribes" do
+    subscribe(room_id:)
+    expect(subscription).to be_confirmed
+    expect(subscription).to have_stream_from(room)
+  end
+
+  it "successfully unsubscribes" do
+    subscribe(room_id:)
+    expect(subscription).to be_confirmed
+    expect(subscription).to have_stream_from(room)
+    subscription.unsubscribe_from_channel
+    expect(subscription).not_to have_streams
+  end
+
+  it "broadcasts a message" do
+    subscribe(room_id:)
+    expect {
+      perform :speak, {message: "Hello, World!"}
+    }.to have_broadcasted_to(room).with(message: "Hello, World!")
+  end
 end
