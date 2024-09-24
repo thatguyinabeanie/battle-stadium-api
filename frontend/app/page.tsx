c5f04fd50d06b10bd3fb195b0bb4f1965dca5b42
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-23 22:34:11 -0500

    lint

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,14 +6,14 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  return(
+  return (
     <div className="inline-block max-w-lg text-center justify-center m-20">
-      <h1 className={ title({ color: "violet", size: "lg" }) }>battlestadium.gg&nbsp;</h1>
+      <h1 className={title({ color: "violet", size: "lg" })}>battlestadium.gg&nbsp;</h1>
 
       <br />
 
-      <h2 className={ title({ size: "sm" }) }>a next-gen tournament website.</h2>
-      <h2 className={ subtitle({ class: "mt-4" }) }>Beautiful, fast and modern. Coming Soon...</h2>
+      <h2 className={title({ size: "sm" })}>a next-gen tournament website.</h2>
+      <h2 className={subtitle({ class: "mt-4" })}>Beautiful, fast and modern. Coming Soon...</h2>
     </div>
-  )
+  );
 }
