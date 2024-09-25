63f8c133712e035830273dbcdbd5954047063ff1
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-24 19:58:31 -0500

    useActionCableConnection

--- frontend/components/chat.tsx
+++ frontend/components/chat.tsx
@@ -2,8 +2,8 @@
 "use client";
 
 import * as React from "react";
-import { createConsumer, Consumer, Subscription, Mixin } from "@rails/actioncable";
 import { Button } from "@nextui-org/react";
+import { useActionCableConnection } from "@/lib/websocket/useActionCableConnection";
 
 export interface Message {
   key: string;
@@ -16,77 +16,9 @@ export interface SpeakData {
   message: string;
 }
 
-type SubscriptionConnection<M> = Subscription<Consumer> &
-  Mixin & {
-    connected(): void;
-    disconnected(): void;
-    received(data: M): void;
-  };
-
-const useCableConnection = <M extends object, S extends object>() => {
-  const [messages, setMessages] = React.useState<M[]>([]);
-  const cableRef = React.useRef<Consumer | null>(null);
-  const connectionRef = React.useRef<SubscriptionConnection<M> | null>(null);
-
-  const sendMessage = (speakData: S, onSuccess?: () => void) => {
-    if (!connectionRef.current) {
-      throw new Error("Connection not established");
-    } else if (connectionRef.current.perform("speak", speakData)) {
-      if (onSuccess) onSuccess();
-
-      return true;
-    }
-  };
-
-  const connectToCable = () => {
-    // Create a consumer
-    const cable = createConsumer("ws://localhost:10000/cable");
-
-    cableRef.current = cable;
-
-    // Create a subscription to the chat channel
-    const subscription = cable.subscriptions.create(
-      { channel: "ChatChannel", room: "1" },
-      {
-        connected() {
-          console.info("Connected to the chat channel"); // eslint-disable-line no-console
-        },
-        disconnected() {
-          console.error("Disconnected from the chat channel"); // eslint-disable-line no-console
-          // Attempt to reconnect after a delay
-          setTimeout(() => {
-            connectToCable();
-          }, 5000);
-        },
-        received(data: M) {
-          setMessages((prevMessages) => [...prevMessages, data]);
-        },
-      },
-    );
-
-    connectionRef.current = subscription;
-  };
-
-  React.useEffect(() => {
-    connectToCable();
-
-    // Cleanup subscription on component unmount
-    return () => {
-      if (connectionRef.current) {
-        connectionRef.current.unsubscribe();
-      }
-      if (cableRef.current) {
-        cableRef.current.disconnect();
-      }
-    };
-  }, []);
-
-  return { messages, sendMessage };
-};
-
 export default function ChatComponent() {
   const [newMessage, setNewMessage] = React.useState("");
-  const { messages, sendMessage } = useCableConnection<Message, SpeakData>();
+  const { messages, sendMessage } = useActionCableConnection<Message, SpeakData>();
 
   const handleSendMessage = async () => {
     sendMessage({ message: newMessage }, () => setNewMessage(""));
