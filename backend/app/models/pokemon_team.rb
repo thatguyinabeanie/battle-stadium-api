e9b851c1cec2c948bc6954d4a7104a445fd962ae
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-26 17:29:06 -0500

    fixed pokemon teams specs

--- backend/app/models/pokemon_team.rb
+++ backend/app/models/pokemon_team.rb
@@ -1,6 +1,6 @@
 # app/models/pokemon_team.rb
 class PokemonTeam < ApplicationRecord
-  belongs_to :profile, class_name: "Profile", optional: true
+  belongs_to :profile, class_name: "Profile", optional: false, inverse_of: :pokemon_teams
   delegate :user , to: :profile
 
   has_many :pokemon, class_name: "Pokemon", inverse_of: :pokemon_team, dependent: :destroy
