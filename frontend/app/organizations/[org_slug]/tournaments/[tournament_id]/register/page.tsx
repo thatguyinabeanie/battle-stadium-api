65bfd9ab0dd4395eff5bb71bd1ed752b1c06b389
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-25 23:06:15 -0500

    register placeholder

--- frontend/app/organizations/[org_slug]/tournaments/[tournament_id]/register/page.tsx
+++ frontend/app/organizations/[org_slug]/tournaments/[tournament_id]/register/page.tsx
@@ -1,27 +1,7 @@
-"use client"
-// todo: refactor using server side compatible form
-
-import { useState } from 'react';
-
-export default function Register () {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    teamName: ''
-  });
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Add logic to submit registration data
-  };
-
+export default function Register() {
   return (
     <div>
       <h1>Register for tournament here</h1>
-      <form onSubmit={ handleSubmit }>
-        {/* Add input fields for name, email, team name */ }
-        <button type="submit">Register</button>
-      </form>
     </div>
   );
 }
