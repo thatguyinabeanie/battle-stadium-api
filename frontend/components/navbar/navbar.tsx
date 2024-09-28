b88f6cb218291679b4765158fd99a09076e1827f
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-28 16:52:19 -0500

    Refactor navbar links component to include dashboard link for authenticated users and adjust styling

--- frontend/components/navbar/navbar.tsx
+++ frontend/components/navbar/navbar.tsx
@@ -20,10 +20,9 @@ import Search from "./search";
 export default function NavigationBar() {
   return (
     <Navbar
-
       isBordered
       classNames={{
-        wrapper: "bg-transparent justify-center backdrop-blur-2xl min-w-full",
+        wrapper: "bg-transparent justify-between backdrop-blur-2xl min-w-full",
         base: " bg-transparent",
         item: "data-[active=true]:text-primary",
         content: "flex-grow-0 h-12 items-center",
@@ -34,12 +33,12 @@ export default function NavigationBar() {
         <BattleStadium />
       </NavbarBrand>
 
-      <NavbarContent className="hidden md:flex px-4 gap-1" data-justify={"center"}>
+      <NavbarContent className="hidden md:flex gap-2 m-x4" data-justify={"center"}>
         <NavbarLinks />
       </NavbarContent>
 
       {/* Right Menu */}
-      <NavbarContent className="gap-1" justify="center">
+      <NavbarContent className="gap-0" justify="center">
         <Search />
         <Settings />
         <Notifications />
