4fffaf6451b3149629ad19b01f8f299e2e3529a6
thatguyinabeanie <thatguyin@beanie.gg>
2024-09-28 23:37:07 -0500

    new cards

---
+++ frontend/app/organizations/[org_slug]/org-detail-card.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Card, CardBody } from "@/components/nextui-use-client";
+import OrganizationLogo from "@/components/organizations/organization-logo";
+import { components } from "@/lib/api/openapi-v1";
+
+interface OrgDetailCardProps {
+  organization: components["schemas"]["Organization"];
+}
+
+export default function OrgDetailCard({ organization }: Readonly<OrgDetailCardProps>) {
+  return (
+    <Card className="bg-transparent h-90 w-90 rounded-3xl" shadow="md">
+      <CardBody className="flex flex-row justify-between rounded-3xl">
+        <OrganizationLogo organization={organization} />
+
+        <div className="flex flex-col justify-between text-center mx-4">
+          <h1 className="text-2xl font-semibold">{organization?.name}</h1>
+          <p>{organization?.description}</p>
+          <p>[ICON LINKS TO SOCIAL MEDIA PROFILES]</p>
+        </div>
+
+        <OrganizationLogo className="hidden sm:flex" organization={organization} />
+      </CardBody>
+    </Card>
+  );
+}
