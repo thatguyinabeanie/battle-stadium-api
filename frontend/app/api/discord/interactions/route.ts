4b0c4ab42b598dacb25d53d8d0f08d9670b085a2
Gabe Mendoza <thatguyin@beanie.gg>
2024-10-07 20:39:13 -0500

    Frontend package updates (#466)
    * frontend-package-updates
    
    * yeet ky
    
    * removed ky
    
    * updated packages
    
    * prettier

--- frontend/app/api/discord/interactions/route.ts
+++ frontend/app/api/discord/interactions/route.ts
@@ -10,7 +10,6 @@ import {
   MessageFlags,
 } from "discord-api-types/v10";
 import { NextRequest, NextResponse } from "next/server";
-import ky from "ky";
 import { nanoid } from "nanoid";
 
 /**
@@ -168,18 +167,22 @@ const createEmbedObject = (source: string, path: string): APIEmbed => {
 const getRandomPic = async (value: RandomPicType) => {
   switch (value) {
     case "cat":
-      const { url } = await ky.get("https://cataas.com/cat?json=true").json<{ url: string }>();
+      const catResponse = await fetch("https://cataas.com/cat?json=true");
+      const catData = await catResponse.json();
+      const { url: catUrl } = catData;
 
-      return { ...createEmbedObject("https://cataas.com", url), description: "Here's a random cat picture!" };
+      return { ...createEmbedObject("https://cataas.com", catUrl), description: "Here's a random cat picture!" };
 
     case "dog":
-      const { message } = await ky.get("https://dog.ceo/api/breeds/image/random").json<{ message: string }>();
+      const dogResponse = await fetch("https://dog.ceo/api/breeds/image/random");
+      const dogData = await dogResponse.json();
+      const { message: dogUrl } = dogData;
 
       return {
         ...baseRandomPicEmbed,
         description: "Here's a random dog picture!",
         fields: [{ name: "source", value: "https://dog.ceo/api" }],
-        image: { url: message },
+        image: { url: dogUrl },
       };
 
     default:
