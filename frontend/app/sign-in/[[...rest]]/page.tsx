4d912196d0c4b17c08357da944199d5893657966
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-09 09:38:42 -0500

    spaces

--- frontend/app/sign-in/[[...rest]]/page.tsx
+++ frontend/app/sign-in/[[...rest]]/page.tsx
@@ -12,9 +12,7 @@ export default function SignInPage() {
 
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-content1 px-8 pb-10 pt-6 shadow-small">
-        <SignIn />
-      </div>
+      <SignIn />
     </div>
   );
 }
