1a1431f4e58cd6f722f347dfe0c9dd772a1d6c40
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-08 20:30:28 -0500

    save

--- frontend/app/api/docs/rails/page.tsx
+++ frontend/app/api/docs/rails/page.tsx
@@ -1,4 +1,5 @@
-import yaml from "js-yaml";
+import SwaggerUI from "swagger-ui-react";
+
 import "swagger-ui-react/swagger-ui.css";
 import { getMe } from "@/app/data/actions";
 import { redirect } from "next/navigation";
@@ -6,7 +7,6 @@ import { auth } from "@clerk/nextjs/server";
 
 import { getBaseUrl } from "@/lib/api";
 import { Card, CardBody } from "@/components/nextui-use-client";
-import SwaggerUI from "swagger-ui-react";
 
 async function fetchOpenApiYaml() {
   const response = await fetch(`${getBaseUrl()}/api-docs/v1/openapi.yaml`);
@@ -27,12 +27,13 @@ export default async function OpenApiDocs() {
     return redirect("/"); // Redirect to home page if user is not an admin)
   }
 
-  const jsonSpec = yaml.load(await fetchOpenApiYaml());
+  // const jsonSpec = yaml.load(await fetchOpenApiYaml());
+  const jsonSpec = '{"openapi": "3.0.1"}';
 
   return (
     <Card className="bg-transparent h-90 w-90 rounded-3xl backdrop-blur-md" shadow="md">
       <CardBody className="flex flex-row justify-between rounded-3xl p-10 ">
-        <SwaggerUI displayOperationId={true} spec={jsonSpec as string} />
+        <SwaggerUI displayOperationId={ true } spec={ jsonSpec } />
       </CardBody>
     </Card>
   );
