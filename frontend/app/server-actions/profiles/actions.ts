1a187eed25794d531ad007e875f4fcc0492d8137
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-13 13:24:56 -0500

    renaming

--- frontend/app/server-actions/profiles/actions.ts
+++ frontend/app/server-actions/profiles/actions.ts
@@ -4,7 +4,7 @@ import { BattleStadiumApiClient, defaultConfig } from "@/lib/api";
 import { paths } from "@/lib/api/openapi-v1";
 import { FetchOptions } from "openapi-fetch";
 
-export async function getProfiles (options?: FetchOptions<paths["/profiles"]["get"]>) {
+export async function getProfiles(options?: FetchOptions<paths["/profiles"]["get"]>) {
   const profilesOptions = {
     ...defaultConfig("getPlayerProfiles"),
     ...options,
