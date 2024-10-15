e95b8699aedd4e860df96a76ec89b0ae7a679dcc
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-15 14:44:05 -0500

    inprogress

--- frontend/components/profiles/new-profile.tsx
+++ frontend/components/profiles/new-profile.tsx
@@ -4,24 +4,18 @@ import React from "react";
 import { Button, Input } from "../nextui-use-client";
 
 export default function NewProfile() {
-
   const [input, setInput] = React.useState<string>("");
 
   const handleSubmit = () => {
-    console.log("submitting new profile", input);
-  }
+    console.log("submitting new profile", input); // eslint-disable-line no-console
+  };
 
   return (
-    <form className="flex flex-row" >
-      <Input
-        placeholder="new profile"
-        name="profile"
-        value={ input }
-        onChange={ (e) => setInput(e.target.value) }
-      />
+    <form className="flex flex-row">
+      <Input name="profile" placeholder="new profile" value={input} onChange={(e) => setInput(e.target.value)} />
       <Button color="primary" type="submit" onClick={handleSubmit}>
         Add Profile
       </Button>
     </form>
-  )
+  );
 }
