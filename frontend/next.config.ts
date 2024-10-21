af5ca6a3b1d1e1fc1c9a25ca9ae29cc88bdfebaa
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-21 17:49:44 -0500

    updating to nsxtjs15. todo / headers were iterated over. headers() should be awaited before using its value.

---
+++ frontend/next.config.ts
@@ -0,0 +1,30 @@
+import type { NextConfig } from "next";
+
+import { join } from "path";
+import dotenv from "dotenv";
+import { withHydrationOverlay } from "@builder.io/react-hydration-overlay/next";
+
+dotenv.config({ path: join(process.cwd(), ".env") });
+dotenv.config({ path: join(process.cwd(), ".env.development.local") });
+
+const nextConfig: NextConfig = {
+  experimental: {
+    after: true,
+  },
+
+  reactStrictMode: true,
+
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
+};
+
+export default withHydrationOverlay({
+  /**
+   * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
+   * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
+   */
+  appRootSelector: "main",
+})(nextConfig);
