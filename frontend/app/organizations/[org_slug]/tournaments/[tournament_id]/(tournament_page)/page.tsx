537d2c4195d8d0e138173d89d1b1aeb0c0d1e07b
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-24 20:53:38 -0500

    lint

--- frontend/app/organizations/[org_slug]/tournaments/[tournament_id]/(tournament_page)/page.tsx
+++ frontend/app/organizations/[org_slug]/tournaments/[tournament_id]/(tournament_page)/page.tsx
@@ -10,7 +10,7 @@ interface OrganizationTournamentProps {
   params: Promise<{
     org_slug: string;
     tournament_id: number;
-  }>
+  }>;
 }
 
 export async function generateMetadata(props: Readonly<OrganizationTournamentProps>) {
