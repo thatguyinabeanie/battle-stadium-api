c02612e106373f7015c3ca4ce368d6347ab50756
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-27 11:45:58 -0500

    coderabbit review feedback

--- backend/spec/spec_helper.rb
+++ backend/spec/spec_helper.rb
@@ -24,16 +24,15 @@ require "simplecov-lcov"
 
 SimpleCov::Formatter::LcovFormatter.config do |c|
   c.report_with_single_file = true
-  c.output_directory = 'coverage'
-  c.lcov_file_name = 'lcov.info'
+  c.output_directory = "coverage"
+  c.lcov_file_name = "lcov.info"
 end
 
-SimpleCov.formatter = SimpleCov::Formatter::Console
-
 SimpleCov.start "rails" do
-
   enable_coverage :branch
+
   formatter SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::Console,
     SimpleCov::Formatter::LcovFormatter,
     SimpleCov::Formatter::HTMLFormatter
   ])
