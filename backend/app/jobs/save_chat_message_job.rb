fc94c3182c3b638cfd88ca59bb3ed446167ff986
thatguyinabeanie <thatguyin@beanie.gg>
2024-10-29 23:33:08 -0500

    undo job changes

--- backend/app/jobs/save_chat_message_job.rb
+++ backend/app/jobs/save_chat_message_job.rb
@@ -2,16 +2,13 @@ class SaveChatMessageJob < ApplicationJob
   queue_as :default
 
   def perform(match_id:, profile_id:, content:, account_id:, sent_at:, message_type:)
-
-
-    Rails.logger.info("HELLO WORLD")
-    # ChatMessage.create!(
-    #   match_id:,
-    #   account_id:,
-    #   profile_id:,
-    #   content:,
-    #   message_type:,
-    #   sent_at:
-    # )
+    ChatMessage.create!(
+      match_id:,
+      account_id:,
+      profile_id:,
+      content:,
+      message_type:,
+      sent_at:
+    )
   end
 end
