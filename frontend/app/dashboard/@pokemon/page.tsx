b5988e23ffc2c3adca4ca97245ff333c2daa79cb
Gabe Mendoza <thatguyin@beanie.gg>
2024-10-30 13:41:02 -0500

    Remove nextjs (#536)
    * yeet nextjs
    
    * cleaning up remnants of frontend
    
    * move backend contents up to root
    
    * update dirs
    
    * update paths
    
    * docker
    
    * cleanup docker compose
    
    * yeet .ruby-version
    
    * add .ruby-versoin
    
    * cleaning up more stuffs
    
    * configured docker compose
    
    * configured docker compose
    
    * configured docker compose
    
    * fix network
    
    * ignore
    
    * cleanup
    
    * seperate bundler install

--- frontend/app/dashboard/@pokemon/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DashboardPokemon() {
-  return (
-    <div>
-      <h1>DASHBOARD POKEMON PAGE</h1>
-    </div>
-  );
-}
--- frontend/components/upload-thing/components.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { generateUploadButton, generateUploadDropzone } from "@uploadthing/react";
-
-import type { UploadThingFileRouter } from "~/app/api/uploadthing/core";
-
-export const UploadButton = generateUploadButton<UploadThingFileRouter>();
-export const UploadDropzone = generateUploadDropzone<UploadThingFileRouter>();
