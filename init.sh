224a1b22b48a2a9cc944baf130a66546c0a8f619
Gabe Mendoza <thatguyin@beanie.gg>
2025-04-09 22:10:45 -0500

    fix some things (#614)
    * fix some things
    
    * fix some things
    
    * spellings
    
    * no more code-rabbit
    
    * nino
    
    * no more printf
    
    * cspell json
    
    * cspell json
    
    * bundle update

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ SECRET=$(openssl rand -base64 48)
 
 # Exit with nonzero status if no arguments are passed in
 if [ $# -eq 0 ]; then
-  echo "No arguments provided. Using default values...\n"
+  echo "No arguments provided. Using default values..."
   postgres_user="postgres"
   postgres_password="postgres"
   postgres_db="fuecoco-db-dev"
@@ -20,34 +20,36 @@ fi
 echo "POSTGRES_USER=$postgres_user"
 echo "POSTGRES_PASSWORD=$postgres_password"
 echo "POSTGRES_DB=$postgres_db"
-echo "POSTGRES_PORT=$postgres_port\n"
+echo "POSTGRES_PORT=$postgres_port"
 
 # POSTGRES ENV FILE SETUP
 if [ -f .env.postgres ]; then
-  echo ".env.postgres exists. skipping...\n"
+  echo ".env.postgres exists. skipping..."
 else
   echo ".Creating .env.postgres ..."
   touch .env.postgres
-  echo "POSTGRES_USER=$postgres_user" >> .env.postgres
-  echo "POSTGRES_PASSWORD=$postgres_password" >> .env.postgres
-  echo "POSTGRES_DB=$postgres_db" >> .env.postgres
-  echo "POSTGRES_PORT=5432" >> .env.postgres
-  echo "done creating .env.postgres\n"
+  {
+    echo "POSTGRES_USER=$postgres_user"
+    echo "POSTGRES_PASSWORD=$postgres_password"
+    echo "POSTGRES_DB=$postgres_db"
+    echo "POSTGRES_PORT=5432"
+  } > .env.postgres
+  echo "done creating .env.postgres"
 fi
 
 # FRONTEND ENV FILE SETUP
 if [ -f frontend/.env ]; then
-  echo "frontend/.env.postgres exists. skipping...\n"
+  echo "frontend/.env.postgres exists. skipping..."
 else
   echo "Creating frontend/.env ..."
   touch frontend/.env
   echo "AUTH_SECRET='$SECRET'" >> frontend/.env
-  echo "done creating frontend/.env\n"
+  echo "done creating frontend/.env"
 fi
 
 # BACKEND ENV FILE SETUP
 if [ -f .env ]; then
-  echo ".env exists. skipping...\n"
+  echo ".env exists. skipping..."
 else
   echo "Creating .env ..."
   touch .env
